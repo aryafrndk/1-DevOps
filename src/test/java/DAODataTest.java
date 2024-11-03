0bf9f0b3fcb61080deee1433bd0a7627c908f7aa
Arya Frandika Daulay <66843350+aryafrndk@users.noreply.github.com>
2024-11-04 00:02:37 +0700

    Update DAODataTest.java

--- src/test/java/DAODataTest.java
+++ src/test/java/DAODataTest.java
@@ -3,6 +3,7 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import DAO.DAOData;  // Import the DAOData class
 import model.TambahData;
 import java.util.List;
 
